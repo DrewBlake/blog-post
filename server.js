7cbebf8b51beabdd94869beba1b1a0230349aba9
Andrew Blake <drewblake1974@gmail.com>
2018-07-20 19:56:44 -0400

    final updates, really

--- server.js
+++ server.js
@@ -6,21 +6,12 @@ const app = express();
 
 const blogPostsRouter = require('./blogPostsRouter');
 
-// log the http layer
+
 app.use(morgan('common'));
+
 app.use(express.json());
-//app.use(express.static('public'));
-
-/*app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html');
-});*/
 
 
-// when requests come into `/shopping-list` or
-// `/recipes`, we'll route them to the express
-// router instances we've imported. Remember,
-// these router instances act as modular, mini-express apps.
-//app.use('/shopping-list', shoppingListRouter);
 app.use('/blog-posts', blogPostsRouter);
 
 app.listen(process.env.PORT || 8080, () => {
